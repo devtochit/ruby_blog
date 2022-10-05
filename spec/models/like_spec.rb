229c2c08ff3645faa9fdd449759e24bcdb39c09b
officialesse <uzomaesse@gmail.com>
2022-10-06 00:14:57 +0100

    [feat] added fix

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  user = User.new(
+    name: 'Gabriel',
+    photo: 'https://www.pexels.com/photo/silhouette-of-a-person-on-a-swing-3293148/',
+    bio: "I'm a student at Microverse",
+    posts_counter: 0
+  )
+
   subject do
-    author = User.new(
-      name: 'Mike Patton',
-      photo: 'http://www.nacionrock.com/wp-content/uploads/patton-34.jpg',
-      bio: 'Singer with one of the widest vocal ranges of all time',
-      posts_counter: 0
+    Like.new(
+      post: Post.new(
+        author: user,
+        title: 'Title',
+        text: 'Text',
+        likes_counter: 0,
+        comments_counter: 0
+      ),
+      author: user
     )
-    post = Post.new(author:,
-                    title: 'Title',
-                    text: 'Text',
-                    comments_counter: 0,
-                    likes_counter: 0)
-    Like.new(post:, author:)
   end
 
-  before { subject.save }
-
-  #  Test methods
-  describe 'update_likes_counter' do
+  describe '#update_likes_counter' do
     it 'should update the likes counter' do
-      expect(subject.send(:update_likes_counter)).to be_valid
+      expect(subject.send(:update_likes_counter).likes_counter).to eq(subject.post.likes_counter)
     end
   end
 end
